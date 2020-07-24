13c74b6c759049b78b5ef29cb641db050526c90a
sizumita <sumito@izumita.com>
2020-07-24 23:01:38 +0900

    [Add]ギルドの設定
    ギルドの設定を保存・変更・ロードするためのclassの作成

--- lib/firestore.py
+++ lib/firestore.py
@@ -1,11 +1,14 @@
 import firebase_admin
 from firestore_guild import Guild
+from firestore_guild_setting import Setting
 from firebase_admin import credentials
 from firebase_admin import firestore
 import os
 import concurrent.futures
 
 cred = credentials.Certificate(os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
+
+# TODO: credentialsをasyncio対応にする
 firebase_admin.initialize_app(cred)
 
 
@@ -19,3 +22,4 @@ class FireStore:
         self.db = firestore.client()
         self.executor = concurrent.futures.ProcessPoolExecutor(max_workers=20)
         self.guild = Guild(self)
+        self.setting = Setting(self)
