672e8076cb37e8a16daeca7eab26f1eb3e44655e
sizumita <sumito@izumita.com>
2020-07-28 12:43:09 +0900

    [Change]細かい変更
    文言の変更

--- cogs/guild_setting.py
+++ cogs/guild_setting.py
@@ -19,10 +19,10 @@ class GuildSetting(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(invoke_with_command=True, aliases=['setting', 'preference'])
+    @commands.group(invoke_without_command=True, aliases=['setting', 'preference'])
     async def pref(self, ctx):
         """ギルドの設定を表示"""
-        data = await self.bot.firestore.guild.get(ctx.guild.id).data()
+        data = await self.bot.firestore.setting.get(ctx.guild.id).data()
         embed = discord.Embed(title=f"{ctx.guild.name} のサーバー設定",
                               color=discord.Color.from_rgb(25, 118, 210))
         embed.add_field(name='ユーザーの名前', value=t_or_f(data.name), inline=False)
@@ -32,9 +32,11 @@ class GuildSetting(commands.Cog):
         embed.add_field(name='読み上げ上限', value=f'{data.limit}文字', inline=False)
         embed.add_field(name='設定コマンド', value=how_to_change.format(prefix=ctx.prefix))
 
+        await ctx.send(embed=embed)
+
     @pref.command()
     async def name(self, ctx):
-        document = self.bot.firestore.guild.get(ctx.guild.id)
+        document = self.bot.firestore.setting.get(ctx.guild.id)
         data = await document.data()
         await document.edit(name=not data.name)
         await ctx.send(
@@ -43,7 +45,7 @@ class GuildSetting(commands.Cog):
 
     @pref.command()
     async def emoji(self, ctx):
-        document = self.bot.firestore.guild.get(ctx.guild.id)
+        document = self.bot.firestore.setting.get(ctx.guild.id)
         data = await document.data()
         await document.edit(emoji=not data.emoji)
         await ctx.send(
@@ -52,7 +54,7 @@ class GuildSetting(commands.Cog):
 
     @pref.command()
     async def bot(self, ctx):
-        document = self.bot.firestore.guild.get(ctx.guild.id)
+        document = self.bot.firestore.setting.get(ctx.guild.id)
         data = await document.data()
         await document.edit(emoji=not data.bot)
         await ctx.send(
@@ -62,16 +64,16 @@ class GuildSetting(commands.Cog):
     @pref.command()
     async def limit(self, ctx, limit=100):
         if limit <= 0 or 2000 < limit:
-            await ctx.send('文字数制限は1から2000の間で指定してください。')
+            await ctx.send('文字数は1から2000の間で指定してください。')
             return
-        document = self.bot.firestore.guild.get(ctx.guild.id)
+        document = self.bot.firestore.setting.get(ctx.guild.id)
         data = await document.data()
         await document.edit(limit=limit)
-        await ctx.send(f'{ctx.author.mention}, 文字数制限を{data.limit}から{limit}に変更しました。')
+        await ctx.send(f'{ctx.author.mention}, 文字数制限を`{data.limit}`文字から`{limit}`文字に変更しました。')
 
     @pref.command()
     async def keep(self, ctx):
-        document = self.bot.firestore.guild.get(ctx.guild.id)
+        document = self.bot.firestore.setting.get(ctx.guild.id)
         data = await document.data()
         await document.edit(emoji=not data.keep)
         b = '再接続する' if data.keep else '再接続しない'
