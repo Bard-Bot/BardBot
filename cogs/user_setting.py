560c106aa5540f118a29a358b9ccb3120f8ba60e
sizumita <sumito@izumita.com>
2020-07-29 22:51:59 +0900

    [Fix]引数のバグ修正

--- cogs/user_setting.py
+++ cogs/user_setting.py
@@ -17,10 +17,10 @@ how_to_change = """
 """
 
 
-async def refresh(self, ctx, document):
-    self.bot.guild_settings.set(ctx.author.id,
-                                await document.data()
-                                )
+async def refresh(bot, ctx, document):
+    bot.guild_settings.set(ctx.author.id,
+                           await document.data()
+                           )
 
 
 def get_types_text(types, with_lower=True):
@@ -34,7 +34,7 @@ async def edit_voice_type(bot, ctx, language, voice_type):
     document = bot.firestore.user.get(ctx.author.id)
     data = await document.data()
     await document.edit(voice={language: voice_type})
-    await refresh(ctx, document)
+    await refresh(bot, ctx, document)
     await ctx.send(f'{ctx.author.mention}, {language}のボイスの設定を{data.voice[language]}から{voice_type}に変更しました。')
 
 
@@ -69,7 +69,8 @@ async def show_voice_setting(bot, ctx):
                     )
     embed.add_field(name='設定コマンド',
                     value=how_to_change.format(prefix=ctx.prefix,
-                                               ja=get_types_text(JA_VOICE_TYPES, False),
+                                               ja=get_types_text(
+                                                   JA_VOICE_TYPES, False),
                                                en=get_types_text(EN_VOICE_TYPES, False)))
 
     await ctx.send(embed=embed)
@@ -79,7 +80,7 @@ async def change_pitch(bot, ctx, pitch):
     document = bot.firestore.user.get(ctx.author.id)
     data = await document.data()
     await document.edit(pitch=pitch)
-    await refresh(ctx, document)
+    await refresh(bot, ctx, document)
     await ctx.send(f'{ctx.author.mention}, ピッチを{data.pitch}から{pitch}に変更しました。')
 
 
@@ -87,7 +88,7 @@ async def change_speed(bot, ctx, speed):
     document = bot.firestore.user.get(ctx.author.id)
     data = await document.data()
     await document.edit(speed=speed)
-    await refresh(ctx, document)
+    await refresh(bot, ctx, document)
     await ctx.send(f'{ctx.author.mention}, ピッチを{data.speed}から{speed}に変更しました。')
 
 
