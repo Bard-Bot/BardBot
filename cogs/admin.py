a5e10c6ac6cd39999fcb14e34de02f033acb883b
sizumita <sumito@izumita.com>
2020-07-30 14:45:15 +0900

    [Add]git pull用のCogの作成

---
+++ cogs/admin.py
@@ -0,0 +1,97 @@
+import asyncio
+import subprocess
+import traceback
+import textwrap
+import io
+from contextlib import redirect_stdout
+from discord.ext import commands
+
+
+class Admin(commands.Cog):
+    """Admin-only commands that make the bot dynamic."""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_result = None
+
+    def cleanup_code(self, content):
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+
+        # remove `foo`
+        return content.strip('` \n')
+
+    async def cog_check(self, ctx):
+        return await self.bot.is_owner(ctx.author)
+
+    async def run_process(self, command):
+        try:
+            process = await asyncio.create_subprocess_shell(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            result = await process.communicate()
+        except NotImplementedError:
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            result = await self.bot.loop.run_in_executor(None, process.communicate)
+
+        return [output.decode() for output in result]
+
+    @commands.command(hidden=True)
+    async def pull(self, ctx):
+        await ctx.send('start pulling...')
+        async with ctx.typing():
+            stdout, stderr = await self.run_process('git pull')
+            last = stderr[-1000:] if stderr else stdout[-1000:]
+            content = '```\n' + last + '\n```'
+            await ctx.send(content)
+
+    @commands.command(pass_context=True, hidden=True, name='eval')
+    async def _eval(self, ctx, *, body: str):
+        """Evaluates a code"""
+
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self._last_result
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except Exception:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
+
+
+def setup(bot):
+    return bot.add_cog(Admin(bot))
