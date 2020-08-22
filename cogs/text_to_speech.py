0de203acc08717a73b9f18d3382fbc3862b940d6
sizumita <sumito@izumita.com>
2020-08-22 09:27:28 +0900

    リファクタリング

--- cogs/text_to_speech.py
+++ cogs/text_to_speech.py
@@ -1,34 +1,39 @@
 from discord.ext import commands
 import discord
 from lib.voice_server import VoiceData
+from typing import TYPE_CHECKING
+from lib.firestore import UserSettingData, SettingData
+from dataclasses import dataclass
+if TYPE_CHECKING:
+    from bard import BardBot
 
 
+@dataclass
 class TextToSpeech(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+    bot: 'BardBot'
 
-    async def set_user_setting(self, user_id):
+    async def set_user_setting(self, user_id: int) -> UserSettingData:
         document = self.bot.firestore.user.get(user_id)
         data = await document.data()
         self.bot.user_settings.set(user_id, data)
 
         return data
 
-    async def set_guild_setting(self, guild_id):
+    async def set_guild_setting(self, guild_id: int) -> SettingData:
         document = self.bot.firestore.setting.get(guild_id)
         data = await document.data()
         self.bot.guild_settings.set(guild_id, data)
 
         return data
 
-    async def set_guild_dict(self, guild_id):
+    async def set_guild_dict(self, guild_id: int) -> dict:
         document = self.bot.firestore.dict.get(guild_id)
         data = await document.data()
         self.bot.guild_dicts.set(guild_id, data)
 
         return data
 
-    async def say(self, message):
+    async def say(self, message: discord.Message) -> None:
         user_setting = self.bot.user_settings.get(message.author.id) or await self.set_user_setting(message.author.id)
         guild_setting = self.bot.guild_settings.get(message.guild.id) or await self.set_guild_setting(message.guild.id)
         guild_dict = self.bot.guild_dicts.get(message.guild.id) or await self.set_guild_dict(message.guild.id)
@@ -44,7 +49,7 @@ class TextToSpeech(commands.Cog):
         await self.bot.voice_manager.get(message.guild.id).put(data)
 
     @commands.Cog.listener(name='on_message')
-    async def text_to_speech(self, message: discord.Message):
+    async def text_to_speech(self, message: discord.Message) -> None:
         if message.content.startswith(';'):
             return
 
@@ -72,5 +77,5 @@ class TextToSpeech(commands.Cog):
         await self.say(message)
 
 
-def setup(bot):
+def setup(bot: 'BardBot'):
     return bot.add_cog(TextToSpeech(bot))
---
+++ lib/firestore/__init__.py
@@ -0,0 +1,5 @@
+from .guild import Guild, GuildData, GuildSnapshot
+from .firestore import FireStore
+from .guild_setting import Setting, SettingData, SettingSnapshot
+from .guild_dict import GuildDict, GuildDictSnapshot
+from .user_setting import UserSetting, UserSettingData, UserSettingSnapshot
